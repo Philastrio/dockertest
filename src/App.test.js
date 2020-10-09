e5b7bd19af2518c3e9a751ecedd660c587bd32e2
philastrio <sideproject0214@gmail.com>
2020-10-09 11:58:57 +0900

    travis yml add

--- src/App.test.js
+++ src/App.test.js
@@ -4,11 +4,5 @@ import App from "./App";
 
 test("renders learn react link", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  console.log(getByText);
 });
